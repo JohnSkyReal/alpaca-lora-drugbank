6e60be11bdc9b471b9f35072596e27b9fd7ddde6
JohnSkyReal <hhtdlam@126.com>
2023-04-04 18:57:54 +0900

    Add files via upload

---
+++ finetune.sh
@@ -0,0 +1,18 @@
+TOT_CUDA="0,1"
+CUDAs=(${TOT_CUDA//,/ })
+CUDA_NUM=${#CUDAs[@]}
+PORT="12345"
+
+DATA_PATH="/kaggle/input/drugbank-alpaca-jsonl/DrugBank_alpaca.jsonl"
+OUTPUT_PATH="lora-alpaca"
+MODEL_PATH="decapoda-research/llama-7b-hf"
+# lora_checkpoint="./lora-Vicuna/checkpoint-11600"
+TEST_SIZE=200
+
+CUDA_VISIBLE_DEVICES=${TOT_CUDA} torchrun --nproc_per_node=$CUDA_NUM --master_port=$PORT finetune.py \
+--data_path $DATA_PATH \
+--output_path $OUTPUT_PATH \
+--model_path $MODEL_PATH \
+--eval_steps 200 \
+--save_steps 200 \
+--test_size $TEST_SIZE
